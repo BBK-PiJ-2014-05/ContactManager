8d0ed1c48a6a2d5a8388c5f64d638da08a0a5a2a
Geoff Watson <geoff777@gmail.com>
2015-03-01 10:36:08 +0000

    Implement Contact

--- src/ContactImpl.java
+++ src/ContactImpl.java
@@ -1,4 +1,53 @@
+/** implementation of Contact interface.
+ * 
+ * @author geoff_000
+ *
+ */
 
-public class ContactImpl {
 
+public class ContactImpl implements Contact{
+
+	private String name;
+	private int id;
+	private String notes;
+	private static int nextId = 0;
+	
+	/** Each contact has a unique ID generated from a static variable. This is the key
+	 * since there may be more than one person with the same name
+	 * 
+	 * @param name
+	 */
+	
+	public ContactImpl(String name){
+		this.name = name;
+		this.id = generateId();
+		this.notes = "";
+		}
+	
+	/**Creation of the key is managed by the class through manipulating the static variable nextId
+	 * 
+	 * @return the unique ID
+	 */
+	
+	private int generateId(){
+		return nextId++;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public String getNotes(){
+		return notes;
+	}
+	
+	public void addNotes(String note){
+		notes = notes + ";"+ note;
+	}
+	
+	
 }
