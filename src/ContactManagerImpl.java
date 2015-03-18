ad231498fdeee9682142790774659c4b43f0d181
Geoff Watson <geoff777@gmail.com>
2015-03-18 23:32:07 +0000

    fixed problems experienced in getFutureMeetingList(contact)

--- src/ContactManagerImpl.java
+++ src/ContactManagerImpl.java
@@ -1,201 +1,206 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Set;
-
 /**
- * Implementation of ContactManager
- * @author geoff_000
- *
- */
+* A class to manage your contacts and meetings.
+*/
 public class ContactManagerImpl implements ContactManager {
-	
-	private ArrayList<Contact> contactList;
-	private Set<Contact> contactSet;
-	private ArrayList<Meeting> futureMeetingList;
-	private ArrayList<PastMeeting> pastMeetingList;
-	Calendar cal;
-	
-	 
-	
-	
-	public ContactManagerImpl(){
-		contactList = new ArrayList<Contact>();
-		contactSet = new HashSet<Contact>();
-		futureMeetingList = new ArrayList<Meeting>();
-		pastMeetingList = new ArrayList<PastMeeting>();	
-	}
-	
-	public int addFutureMeeting(Set<Contact> contacts, Calendar date){
-		Date meetingDate = date.getTime();
-		Calendar calNow = Calendar.getInstance();
-		Date now = calNow.getTime();
-		if (meetingDate.before(now)) {
-			throw new IllegalArgumentException("Error: Date is in the past");
-		}
-		FutureMeeting fm = new FutureMeetingImpl(contacts,date);
-		System.out.println("<Meeting ID: " + fm.getId() + ">");
-		futureMeetingList.add(fm);
-		return fm.getId();	
-	}
-	
-	public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text){
-		PastMeeting pastMeeting = new PastMeetingImpl(contacts,date,text);
-		System.out.println("<Meeting ID: " + pastMeeting.getId() + ">");
-		pastMeetingList.add(pastMeeting);
-		if (date == null || contacts == null || text == null) {
-			throw new NullPointerException("parameter(s) missings");
-		}
-	}
-	
-	public void addMeetingNotes(int id, String text){
-		for (int i = 0; i < pastMeetingList.size(); i++){
-			if (pastMeetingList.get(i).getId() == id){
-				
-			}
-		}
-		
-	}
 
-	public PastMeeting getPastMeeting(int id){
-		for (int i = 0; i < pastMeetingList.size(); i++) {
-			if (pastMeetingList.get(i).getId() == id){
-				return pastMeetingList.get(i);
-			} else {
-			} 
-		} return null;
-	}
+	List<Meeting> futureMeetingList = new ArrayList<Meeting>();
+	List<Contact> contactList = new ArrayList<Contact>();
+	List<Meeting> meetingList = new ArrayList<Meeting>();
 	
-	public FutureMeeting getFutureMeeting(int id){
-		boolean found = false;
-		for (int i = 0; i < futureMeetingList.size(); i++){
-			if (futureMeetingList.get(i).getId() == id){
-					Calendar cal = futureMeetingList.get(i).getDate();
-					Date meetingDate = cal.getTime();
-					Calendar calNow = Calendar.getInstance();
-					Date now = calNow.getTime();
-					if (meetingDate.before(now)){
-						throw new IllegalArgumentException("Date is in the past");
-					}
-				
-				System.out.println("found it!");
-				found = true;
-				System.out.println("Date: " + futureMeetingList.get(i).getDate());
-				System.out.println("Attendees: " + futureMeetingList.get(i).getContacts());
-				Meeting meeting = futureMeetingList.get(i);
-				FutureMeeting futureMeeting  = (FutureMeeting)meeting;
-				return futureMeeting;
-			}
-		}
-		if (!found) {
-			System.out.println("meeting not found");
-		}
-		return null;
-	}
-	
-	public Meeting getMeeting(int id){
-		for (int i = 0; i < futureMeetingList.size(); i++){
-			if (futureMeetingList.get(i).getId() == id){
-				return futureMeetingList.get(i);
-			} 
-		}
-			for (int j = 0; j < pastMeetingList.size(); j++){
-			 if (pastMeetingList.get(j).getId() == id){
-				return pastMeetingList.get(j);
-			} 
-			}
-			return null;	
-	}
-	
-	public List<Meeting> getFutureMeetingList(Contact contact){
-		List<Meeting> contactMeeting = new ArrayList<Meeting>();
-		for (int i = 0; i < futureMeetingList.size(); i++){
-				if (futureMeetingList.get(i).getContacts().contains(contact)){
-					contactMeeting.add(futureMeetingList.get(i));
-					System.out.println("found contact at i= " + i);
-				} else { System.out.println("else case, i = " + i);
-				i++;
-				
-			}
-		}
-		System.out.println("contactMeetingList size" + contactMeeting.size());	
-		return contactMeeting;
-	}
-	/**
-	 * Test to get size of fmlist--remove
-	 * @return
-	 */
-	public int getFutureMeetingListSize(){
-		return futureMeetingList.size();
-	}
 	
 	/**
-	 * test to get size
-	 */
-	public int getContactSetSize(){
-		return contactSet.size();
+* Add a new meeting to be held in the future.
+*
+* @param contacts a list of contacts that will participate in the meeting
+* @param date the date on which the meeting will take place
+* @return the ID for the meeting
+* @throws IllegalArgumentException if the meeting is set for a time in the past,
+* of if any contact is unknown / non-existent
+*/
+public int addFutureMeeting(Set<Contact> contacts, Calendar date){
+	Date meetingDate = date.getTime();
+	Calendar calNow = Calendar.getInstance();
+	Date now = calNow.getTime();
+	if (meetingDate.before(now)) {
+		throw new IllegalArgumentException("Error: Date is in the past");
+	}
+	Meeting futureMeeting = new FutureMeetingImpl(contacts,date);
+	futureMeetingList.add(futureMeeting);
+	return futureMeeting.getId();
+}
+/**
+1* Returns the PAST meeting with the requested ID, or null if it there is none.
+*
+* @param id the ID for the meeting
+* @return the meeting with the requested ID, or null if it there is none.
+* @throws IllegalArgumentException if there is a meeting with that ID happening in the future
+*/
+public PastMeeting getPastMeeting(int id){
+	return null;
+}
+/**
+* Returns the FUTURE meeting with the requested ID, or null if there is none.
+*
+* @param id the ID for the meeting
+* @return the meeting with the requested ID, or null if it there is none.
+* @throws IllegalArgumentException if there is a meeting with that ID happening in the past
+*/
+public FutureMeeting getFutureMeeting(int id){
+	for (int i = 0; i < futureMeetingList.size();i++){
+		Meeting meeting = futureMeetingList.get(i);
+		if (meeting.getId() == id){
+			System.out.print("found");
+			FutureMeeting futureMeeting = (FutureMeeting)meeting;
+			return futureMeeting;
+		}
+	} return null;
+}
+/**
+* Returns the meeting with the requested ID, or null if it there is none.
+*
+* @param id the ID for the meeting
+* @return the meeting with the requested ID, or null if it there is none.
+*/
+public Meeting getMeeting(int id){
+	return null;
+}
+/**
+* Returns the list of future meetings scheduled with this contact.
+*
+* If there are none, the returned list will be empty. Otherwise,
+* the list will be chronologically sorted and will not contain any
+* duplicates.
+*
+* @param contact one of the user’s contacts
+* @return the list of future meeting(s) scheduled with this contact (maybe empty).
+* @throws IllegalArgumentException if the contact does not exist
+*/
+public List<Meeting> getFutureMeetingList(Contact contact){
+	List<Meeting> list = new ArrayList<Meeting>();
+	for (int i = 0; i < futureMeetingList.size(); i++){
+		Set<Contact> contactSet = new HashSet<Contact>();
+		contactSet = futureMeetingList.get(i).getContacts();
+		if (contactSet.contains(contact)){
+			list.add(futureMeetingList.get(i));
+		}
+		
 	}
 	
-	public List<Meeting> getFutureMeetingList(Calendar date){
-		return null;
-	}
+	return list;
+}
+/**
+* Returns the list of meetings that are scheduled for, or that took
+* place on, the specified date
+*
+* If there are none, the returned list will be empty. Otherwise,
+* the list will be chronologically sorted and will not contain any
+* duplicates.
+*
+* @param date the date
+* @return the list of meetings
+*/
+public List<Meeting> getFutureMeetingList(Calendar date){
+	return null;
+}
+/**
+* Returns the list of past meetings in which this contact has participated.
+*
+* If there are none, the returned list will be empty. Otherwise,
+* the list will be chronologically sorted and will not contain any
+2* duplicates.
+*
+* @param contact one of the user’s contacts
+* @return the list of future meeting(s) scheduled with this contact (maybe empty).
+* @throws IllegalArgumentException if the contact does not exist
+*/
+public List<PastMeeting> getPastMeetingList(Contact contact){
+	return null;
+}
+/**
+* Create a new record for a meeting that took place in the past.
+*
+* @param contacts a list of participants
+* @param date the date on which the meeting took place
+* @param text messages to be added about the meeting.
+* @throws IllegalArgumentException if the list of contacts is
+* empty, or any of the contacts does not exist
+* @throws NullPointerException if any of the arguments is null
+*/
+public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text){
 	
-	public List<PastMeeting> getPastMeetingList(Contact contact){
-		return null;
-	}
+}
+/**
+* Add notes to a meeting.
+*
+* This method is used when a future meeting takes place, and is
+* then converted to a past meeting (with notes).
+*
+* It can be also used to add notes to a past meeting at a later date.
+*
+* @param id the ID of the meeting
+* @param text messages to be added about the meeting.
+* @throws IllegalArgumentException if the meeting does not exist
+* @throws IllegalStateException if the meeting is set for a date in the future
+* @throws NullPointerException if the notes are null
+*/
+public void addMeetingNotes(int id, String text){
 	
-	public void addNewContact(String name, String notes){
-		Contact c = new ContactImpl(name,notes);
-		contactList.add(c);
-		System.out.println("Added: " + c.getName() + "  <ID: " + c.getId()+ ">");
+}
+/**
+* Create a new contact with the specified name and notes.
+*
+* @param name the name of the contact.
+* @param notes notes to be added about the contact.
+* @throws NullPointerException if the name or the notes are null
+*/
+public void addNewContact(String name, String notes){
+	if (name == null || notes == null){
+		throw new NullPointerException("Name or notes are null");
 	}
-	
-	public Set<Contact> getContacts(int... ids){
-		contactSet.clear();
-		for(int i = 0; i < ids.length; i++){
-			for (int x = 0; x < contactList.size();x++){
-				if(contactList.get(x).getId() == (ids[i])){
-					contactSet.add(contactList.get(x));
-				}
+	Contact contact = new ContactImpl(name,notes);
+	contactList.add(contact);
+}
+/**
+* Returns a list containing the contacts that correspond to the IDs.
+*
+* @param ids an arbitrary number of contact IDs
+* @return a list containing the contacts that correspond to the IDs.
+* @throws IllegalArgumentException if any of the IDs does not correspond to a real contact
+*/
+public Set<Contact> getContacts(int... ids){
+	Set<Contact> contactSet = new HashSet<Contact>();
+	for(int i = 0; i < ids.length; i++){
+		for (int j = 0; j < contactList.size(); j++){
+			if(contactList.get(j).getId() == (ids[i])){
+				contactSet.add(contactList.get(j));
 			}
 		}
-		Iterator<Contact> iterator = contactSet.iterator(); 
-		while(iterator.hasNext()){
-			Contact element = (Contact) iterator.next();
-			System.out.println(element.getName() + " <ID: " + element.getId()+">");
-		}
-		return contactSet;
 	}
-		
-	public Set<Contact> getContacts(String name){
-		contactSet.clear();
-		boolean found = false;
-		for(int i = 0; i < contactList.size(); i++){
-			if(contactList.get(i).getName().equals(name)){
-				contactSet.add(contactList.get(i));
-				found = true;
-			}
-		}
-		if (!found){
-			System.out.println("Contact not found");
-		}
-		return contactSet;
-	}
-		
-	public void flush(){
-		
-	}
+	return contactSet;
+}
+/**
+* Returns a list with the contacts whose name contains that string.
+3*
+* @param name the string to search for
+* @return a list with the contacts whose name contains that string.
+* @throws NullPointerException if the parameter is null
+*/
+public Set<Contact> getContacts(String name){
+	return null;
+}
+/**
+* Save all data to disk.
+*
+* This method must be executed when the program is
+* closed and when/if the user requests it.
+*/
+public void flush(){
 	
-	
-	
-		
-
-	
+}
 }
